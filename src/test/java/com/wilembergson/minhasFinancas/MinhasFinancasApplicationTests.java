c364223589396dd08fb42b864c51680db2e91e9b
wilembergson <wilembergson@yahoo.com>
2021-07-09 00:01:20 -0300

    Inicio

---
+++ src/test/java/com/wilembergson/minhasFinancas/MinhasFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilembergson.minhasFinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
