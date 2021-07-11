9083e422838551a066b10aa62968223dc72d70e0
wilembergson <wilembergson@yahoo.com>
2021-07-11 17:59:28 -0300

    Testes para salvar usuario

--- src/test/java/com/wilembergson/minhasFinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/wilembergson/minhasFinancas/service/UsuarioServiceTest.java
@@ -9,10 +9,11 @@ import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -23,14 +24,28 @@ import java.util.Optional;
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
 
-    UsuarioService service;
+    @SpyBean
+    UsuarioServiceImpl service;
 
     @MockBean
     UsuarioRepository repository;
 
-    @Before
-    public void setUp(){
-        service = new UsuarioServiceImpl(repository);
+    @Test
+    public void deveSalvarUmUsuario(){
+        //Cenario
+        Mockito.doNothing().when(service).validarEmail(Mockito.anyString());
+        Usuario usuario = Usuario.builder().id(1l).nome("nome").email("email@email.com").senha("senha").build();
+        Mockito.when(repository.save(Mockito.any(Usuario.class))).thenReturn(usuario);
+
+        //Ação
+        Usuario usuarioSalvo = service.salvarUsuario(new Usuario());
+
+        //Verificação
+        Assertions.assertThat(usuarioSalvo).isNotNull();
+        Assertions.assertThat(usuarioSalvo.getId()).isEqualTo(1l);
+        Assertions.assertThat(usuarioSalvo.getNome()).isEqualTo("nome");
+        Assertions.assertThat(usuarioSalvo.getEmail()).isEqualTo("email@email.com");
+        Assertions.assertThat(usuarioSalvo.getSenha()).isEqualTo("senha");
     }
 
     @Test(expected = Test.None.class)
@@ -47,6 +62,20 @@ public class UsuarioServiceTest {
 
         //Verificação
         Assertions.assertThat(result).isNotNull();
+    }
+
+    @Test(expected = RegraNegocioException.class)
+    public void naoDeveCadastrarUmUsuarioComEmailJaCadastrado(){
+        //Cenario
+        String email = "email@email.com";
+        Usuario usuario = Usuario.builder().email(email).build();
+        Mockito.doThrow(RegraNegocioException.class).when(service).validarEmail(email);
+
+        //Ação
+        service.salvarUsuario(usuario);
+
+        //Verificação
+        Mockito.verify(repository, Mockito.never()).save(usuario);
     }
 
     @Test
