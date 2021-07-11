62ab0f767a0bb67b85bc9886cdcc8a9546eeebe6
wilembergson <wilembergson@yahoo.com>
2021-07-11 20:31:38 -0300

    UsuarioController implementado

---
+++ src/main/java/com/wilembergson/minhasFinancas/api/resource/UsuarioController.java
@@ -0,0 +1,39 @@
+package com.wilembergson.minhasFinancas.api.resource;
+
+import com.wilembergson.minhasFinancas.api.dto.UsuarioDTO;
+import com.wilembergson.minhasFinancas.exceptions.RegraNegocioException;
+import com.wilembergson.minhasFinancas.model.entity.Usuario;
+import com.wilembergson.minhasFinancas.service.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    private UsuarioService service;
+
+    public UsuarioController(UsuarioService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody UsuarioDTO dto){
+
+        Usuario usuario = Usuario.builder()
+                                .nome(dto.getNome())
+                                .email(dto.getEmail())
+                                .senha(dto.getSenha())
+                                .build();
+        try{
+            Usuario usuarioSalvo = service.salvarUsuario(usuario);
+            return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+        }catch (RegraNegocioException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
