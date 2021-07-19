69eca9f53a65c7ad0e6a7c704defb4d81b2052a0
wilembergson <wilembergson@yahoo.com>
2021-07-19 01:15:06 -0300

    Construtores em LancamentosDTO

--- src/main/java/com/wilembergson/minhasFinancas/api/dto/LancamentoDTO.java
+++ src/main/java/com/wilembergson/minhasFinancas/api/dto/LancamentoDTO.java
@@ -1,12 +1,16 @@
 package com.wilembergson.minhasFinancas.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LancamentoDTO {
 
     private Long id;
