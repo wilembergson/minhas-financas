10bdf2f5c13d3eb2550cb5ce04b307e4d53ef5d3
wilembergson <wilembergson@yahoo.com>
2021-07-20 02:43:12 -0300

    Converter em DTO

--- src/main/java/com/wilembergson/minhasFinancas/api/resource/LancamentoController.java
+++ src/main/java/com/wilembergson/minhasFinancas/api/resource/LancamentoController.java
@@ -101,6 +101,26 @@ public class LancamentoController {
         return ResponseEntity.ok(lancamentos);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity obterLancamento(@PathVariable("id") Long id){
+        return service.obterPorId(id)
+                    .map(lancamento -> new ResponseEntity(converter(lancamento),HttpStatus.OK))
+                    .orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+    }
+
+    private LancamentoDTO converter(Lancamento lancamento){
+        return LancamentoDTO.builder()
+                            .id(lancamento.getId())
+                            .descricao(lancamento.getDescricao())
+                            .valor(lancamento.getValor())
+                            .mes(lancamento.getMes())
+                            .ano(lancamento.getAno())
+                            .status(lancamento.getStatus().name())
+                            .tipo(lancamento.getTipo().name())
+                            .usuario(lancamento.getUsuario().getId())
+                            .build();
+    }
+
     private Lancamento converter(LancamentoDTO dto){
         Lancamento lancamento = new Lancamento();
         lancamento.setId(dto.getId());
